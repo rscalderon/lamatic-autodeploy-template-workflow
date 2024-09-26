94bf2be745972d872175fe67ec61b75b12842e19
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-09-26 19:10:12 -0400

    Root layour

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: 'Deploy with Lamatic.ai',
+  description: 'How to deploy with Lamatic',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <head>
+        <link
+          href='https://cdn.prod.website-files.com/65ef2805bd1160e6402b86cd/665161461bb44b76af1aaeb8_favicon-32x32.png'
+          rel='shortcut icon'
+          type='image/x-icon'
+        ></link>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
