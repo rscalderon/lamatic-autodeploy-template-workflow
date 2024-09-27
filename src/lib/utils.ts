16c689d6133ea8c00ed11146b145c707bd9da700
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-09-27 12:43:07 -0400

    utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,42 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { variablesCollection } from './types';
+import { adjectives, animals } from './random-names-seed';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function fillTemplateAndReturn(
+  variables: variablesCollection,
+  file: string
+) {
+  let newFile = '';
+
+  for (let i = 0; i < file.length; i++) {
+    if (file[i] === '*') {
+      i++;
+      let newVar = '';
+      while (file[i] !== '*') {
+        newVar += file[i++];
+      }
+      newVar = variables[newVar] || newVar;
+      newFile += newVar;
+    } else {
+      newFile += file[i];
+    }
+  }
+  return newFile;
+}
+
+export const randomNameGenerator = () => {
+  const randomOne = adjectives[Math.round(Math.random() * adjectives.length)];
+  const randomTwo = animals[Math.round(Math.random() * animals.length)];
+  let output = randomOne + '-' + randomTwo;
+  if (randomTwo === randomOne) {
+    const randomThree = animals[Math.round(Math.random() * animals.length)];
+    output = randomOne + '-' + randomThree;
+  }
+
+  return output;
+};
